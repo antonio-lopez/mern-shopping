43197241ff48366e72b78a11f6b6d76e9cac2405
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2021-09-07 18:42:26 -0700

    styled product and added media query

--- frontend/src/components/HomeScreen/HomeScreen.js
+++ frontend/src/components/HomeScreen/HomeScreen.js
@@ -1,6 +1,22 @@
 import './HomeScreen.css';
+import Product from '../Product/Product';
+
 const HomeScreen = () => {
-  return <div className='homescreen'>Home Screen</div>;
+  return (
+    <div className='homescreen'>
+      <h2 className='homescreen__title'>Latest Products</h2>
+      <div className='homescreen__products'>
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 };
 
 export default HomeScreen;
