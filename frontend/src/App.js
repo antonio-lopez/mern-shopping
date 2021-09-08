4cce8f178fbb5f99264c32d96a39d10c85f89c42
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2021-09-07 17:51:58 -0700

    added and styled sidedrawer
    screen responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import HomeScreen from './components/HomeScreen/HomeScreen';
 import ProductScreen from './components/ProductScreen/ProductScreen';
 import CartScreen from './components/CartScreen/CartScreen';
 import Navbar from './components/Navbar/Navbar';
+import Backdrop from './components/Backdrop/Backdrop';
+import SideDrawer from './components/SideDrawer/SideDrawer';
 
 function App() {
+  const [sideToggle, setSideToggle] = useState(false);
   return (
     <Router>
-      <Navbar />
+      <Navbar click={() => setSideToggle(true)} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
+      <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
       <main>
         <Switch>
           <Route exact path='/' component={HomeScreen}></Route>
