11ca911f7b570131739a76841fd9ca0a403a7a92
Antonio Lopez <36496887+antonio-lopez@users.noreply.github.com>
2021-09-12 19:51:41 -0700

    created redux files and rendered hard coded products

--- frontend/src/components/CartScreen/CartScreen.js
+++ frontend/src/components/CartScreen/CartScreen.js
@@ -1,6 +1,25 @@
 import './CartScreen.css';
+import CartItem from '../CartItem/CartItem';
+
 const CartScreen = () => {
-  return <div className='cartscreen'>Cart Screen</div>;
+  return (
+    <div className='cartscreen'>
+      <div className='cartscreen__left'>
+        <h2>Shopping Cart</h2>
+        <CartItem />
+      </div>
+
+      <div className='cartscreen__right'>
+        <div className='cartscreen__info'>
+          <p>Subtotal (0) items</p>
+          <p>$499.99</p>
+        </div>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartScreen;
--- frontend/src/components/HomeScreen/HomeScreen.js
+++ frontend/src/components/HomeScreen/HomeScreen.js
@@ -1,19 +1,29 @@
 import './HomeScreen.css';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProducts as listProducts } from '../../redux/actions/productAction';
 import Product from '../Product/Product';
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <div className='homescreen'>
       <h2 className='homescreen__title'>Latest Products</h2>
       <div className='homescreen__products'>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
+          <h2>{error}</h2>
+        ) : (
+          products.map((product) => <Product />)
+        )}
       </div>
     </div>
   );
